9b27769101476c20d08054e9b266ef3e56863c06
Carlos Gabriel <carlosgabrielpc@live.com>
2022-03-02 23:55:13 -0300

    feat: list and post room messages

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib import messages
@@ -6,12 +7,11 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 
 
 def loginPage(request):
-
     page = 'login'
 
     if request.user.is_authenticated:
@@ -77,7 +77,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 
